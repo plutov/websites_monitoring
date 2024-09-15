b3b34826f313b81a03830a65f4640002e9da0053
Alex Pliutau <a.pliutau@gmail.com>
2024-09-15 23:34:33 +0200

    Add ppx_deriving_yaml

--- lib/config.ml
+++ lib/config.ml
@@ -1,19 +1,19 @@
 type website = {
   url : string;
   interval: int;
-  pattern: string;
-}
+  pattern: string; [@default ""]
+} [@@deriving yaml]
 
-let read_lines filename =
-  let contents = In_channel.with_open_bin filename In_channel.input_all in
-  String.split_on_char '\n' contents
+type websites = {
+  websites: website list;
+} [@@deriving yaml]
 
 let get_websites_from_file(config_filename: string) : website list =
-  let y = Yaml_unix.of_file Fpath.(v config_filename) in
-  let x = match y with
-    | Ok v -> v
-    | Error _ -> raise (Invalid_argument "Invalid yaml config file")
-  in
-  let s = Yaml.to_string_exn x in
-  (* return list with one element*)
-  [ { url = s; interval = 10; pattern = "google" } ]
+  let f = Yaml_unix.of_file Fpath.(v config_filename) in
+  match f with
+  | Ok value ->
+    (match websites_of_yaml value with
+    | Ok t ->
+      t.websites
+    | Error `Msg e -> raise (Invalid_argument ("Invalid config format: " ^ e)))
+  | Error `Msg e -> raise (Invalid_argument ("Unable to parse yaml file: " ^ e))
